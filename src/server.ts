5a1b4cad6aaa8399ec801269a37527bd8f83c7f9
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-11-01 18:32:17 -0300

    model and id generate error

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,38 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
-async function bootstrap() {
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.$connect();
+
   const fastify = Fastify({
     logger: true,
+  });
+
+  fastify.get("/users", () => {
+    return { user: "teste 1" };
+  });
+
+  fastify.post("/users", async () => {
+    const post = await prisma.user.create({
+      data: {
+        name: "João",
+        email: "joaorjoaquim@teste.com",
+        password: "123456",
+      },
+    });
+    return { post };
   });
 
   await fastify.listen({ port: 3333 });
 }
 
-bootstrap();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+  });
