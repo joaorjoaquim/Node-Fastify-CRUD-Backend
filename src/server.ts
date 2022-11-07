04b44b25fef0849f2d6119eb7ee1779fcf3e40b7
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-11-07 17:52:34 -0300

    server update

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import Fastify from "fastify";
+import { projectRoutes } from "./app/routes/project";
+import { taskRoutes } from "./app/routes/task";
 import { userRoutes } from "./app/routes/user";
 import dbConnector from "./database";
 const mongoose = require("../src/database");
@@ -11,6 +13,8 @@ async function main() {
   fastify.register(dbConnector);
 
   await fastify.register(userRoutes);
+  await fastify.register(taskRoutes);
+  await fastify.register(projectRoutes);
 
   fastify.post("/users", async () => {
     return { post: "ok" };
