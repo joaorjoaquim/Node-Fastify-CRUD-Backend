40550464f18b114a86e2b20acd4c91d1af518bb9
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-11-08 18:26:19 -0300

    login route, creating token

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -1,4 +1,13 @@
-import { FastifyInstance } from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import fastify, { FastifyInstance } from "fastify";
+import { z } from "zod";
+const jwt = require("@fastify/jwt");
+
+function generateToken(params = {}) {
+  return jwt.sign(params, process.env.SECRET, {
+    expiresIn: 86400,
+  });
+}
 
 export async function userRoutes(fastify: FastifyInstance) {
   const collection = fastify.mongo.db?.collection("users");
@@ -6,5 +15,21 @@ export async function userRoutes(fastify: FastifyInstance) {
   fastify.get("/users", async () => {
     const result = await collection.find().toArray();
     return { result };
+  });
+
+  fastify.post("/login", async (request, reply) => {
+    const createUserBody = z.object({
+      email: z.string(),
+      password: z.string(),
+    });
+
+    const { email, password } = createUserBody.parse(request.body);
+    const crypted = await reply.jwtSign({ id: email }, { expiresIn: 86400 });
+    console.log(crypted);
+    reply.send({
+      email,
+      password,
+      token: crypted,
+    });
   });
 }
