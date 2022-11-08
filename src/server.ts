79c32773c7ff11d9d8e7857d68327ace50441696
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-11-08 14:35:35 -0300

    setting jwt

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import "dotenv/config";
 import { projectRoutes } from "./app/routes/project";
 import { taskRoutes } from "./app/routes/task";
 import { userRoutes } from "./app/routes/user";
+
 import dbConnector from "./database";
-const mongoose = require("../src/database");
 
 async function main() {
   const fastify = Fastify({
     logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true,
+  });
+
+  await fastify.register(jwt, {
+    secret: process.env.SECRET,
   });
 
   fastify.register(dbConnector);
@@ -15,10 +26,6 @@ async function main() {
   await fastify.register(userRoutes);
   await fastify.register(taskRoutes);
   await fastify.register(projectRoutes);
-
-  fastify.post("/users", async () => {
-    return { post: "ok" };
-  });
 
   await fastify.listen({ port: 3333 });
 }
