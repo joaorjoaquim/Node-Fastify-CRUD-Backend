30fa51bcf9844decf6711ab98615c198a08b8cd8
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-11-16 18:40:50 -0300

    yet to implement the logic of the login

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -29,17 +29,11 @@ export async function userRoutes(fastify: FastifyInstance) {
     const hash = await bcrypt.hash(password, 10);
     const user = await userCollection.findOne({ email });
     let test1 = false;
-    let test2 = false;
 
     if (!(await bcrypt.compare(password, user.password))) {
-      test1 = true;
-    } else {
       test1 = false;
-    }
-    if (!(await bcrypt.compare(hash, user.password))) {
-      test2 = true;
     } else {
-      test2 = false;
+      test1 = true;
     }
 
     const crypted = fastify.jwt.sign(
@@ -60,7 +54,6 @@ export async function userRoutes(fastify: FastifyInstance) {
       token: crypted,
       user,
       test1,
-      test2,
     });
   });
 
